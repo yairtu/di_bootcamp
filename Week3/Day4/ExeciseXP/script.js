ea5c1b467f752c6e439e13ffe4f18c9da3e627ab
yairtu <skiracer131@gmail.com>
2022-01-12 22:33:16 +0200

    week3 day4 daily challenge

---
+++ Week3/Day4/ExeciseXP/script.js
@@ -0,0 +1,49 @@
+//Exercise 1
+
+/*
+let x = 5;
+let y = 3;
+
+if(x < y) {
+    console.log("x is the biggest number");
+}
+
+//Exercise 2
+let newDog = "Chihuahua";
+
+console.log(newDog.length);
+
+console.log(newDog.toUpperCase());
+console.log(newDog.toLowerCase());
+
+if (newDog === "Chihuahua") {
+    console.log("I love Chihuahuas, it’s my favorite dog breed");
+} else {
+    console.log("I dont care, I prefer cats");
+}
+
+
+//Exercise 3
+let num = parseInt(prompt("Enter a number"));
+
+if (num % 0) {
+    prompt(`${num} is an even number`);
+} else {
+    prompt(`${num} is an odd number`);
+}
+
+*/
+
+//Exercise 4
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+
+switch (users.length) {
+    case 0:
+        console.log("no one is online");
+        break;
+    case 1:
+        console.log(`${users[0]} is online`);
+        break;
+    default:
+        console.log(`${users[0]}, ${users[1]} and ${users.length - 2} more are online`);
+}
