c3562041f30ffb25808c436b7fc9c8cae0ca4cf9
yairtu <skiracer131@gmail.com>
2022-02-21 23:39:27 +0200

    week9 day2 daily challenge

--- Week9/Day2/DailyChallenge/daily_challenge.py
+++ Week9/Day2/DailyChallenge/daily_challenge.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Circle:
 
 	def __init__(self, radius):
@@ -11,8 +12,31 @@ class Circle:
 	def __add__(self, other):
 		return self.__pow__() + other.__pow__()
 
+	def print_circle(self):
+		print(f"You have a very nice circle with an area of {self.__pow__()}")
 
-circle = Circle(5)
-circle2 = Circle(2)
+	def compare(self, other):
+		'''returns boolean of if self is greater than other.'''
+		if isinstance(other, Circle):
+			if self.__pow__() == other.__pow__():
+				return "Equal"
+			else:
+				return self.__pow__() > other.__pow__()
 
-print(circle.__add__(circle2))
+	# def sort_circle_list(self, ls):
+	# 	for circle in ls:
+	# 		if circle
+
+
+circle1 = Circle(2)
+circle2 = Circle(5)
+circle3 = Circle(3)
+circle4 = Circle(1)
+
+print(circle1.__add__(circle2))
+circle2.print_circle()
+print(circle2.compare(circle1))
+
+# circles_list = [circle1, circle2, circle3, circle4]
+# for circles in circles_list:
+# 	if circles_list.__pow__()
